50bb01e2754adf7465174ff2de4a63f1d6a0290a
Leo Freitas <leomsfreitas@gmail.com>
2026-04-06 22:12:27 -0300

    feat: replace XGBoost to RandomForestClassifier

--- configs/model_cfg.py
+++ configs/model_cfg.py
@@ -1,12 +1,8 @@
-from xgboost import XGBClassifier
+from sklearn.ensemble import RandomForestClassifier
 
-MODEL = XGBClassifier(
-    n_estimators=200,
-    max_depth=6,
-    learning_rate=0.05,
-    subsample=0.8,
-    colsample_bytree=0.8,
-    eval_metric="logloss"
+MODEL = RandomForestClassifier(
+    class_weight="balanced",
+    n_jobs=-1,
 )
 
-MODEL_NAME = "xgboost"
+MODEL_NAME = "RandomForest"
